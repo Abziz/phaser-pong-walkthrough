4cfa91cd8aa87799be6787da8fa1624ec2747c32
talabziz <abziz89@gmail.com>
2019-05-18 01:01:33 +0300

    deployment preps

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
 const express = require("express");
-const Bundler = require("parcel-bundler");
-const port = 3000;
+const port = process.env.PORT || 3000;
 const server = express();
-const bundler = new Bundler("client/index.html");
 
 server.use("/assets", express.static("assets"));
-server.use(bundler.middleware());
+
+if (process.env.NODE_ENV == "production") {
+	//production -> serve our app from the dist folder
+	server.use("/", express.static("dist"));
+} else {
+	//development -> let parcel do the work
+	const Bundler = require("parcel-bundler");
+	const bundler = new Bundler("client/index.html");
+	server.use(bundler.middleware());
+}
+
 server.listen(port, () => {
 	console.log(`server is running on http://localhost:${port}`);
-})
+});
